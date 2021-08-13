a99debc28de4afe1676cc78a873aed142c7a28d5
zchatt <zacchatt@gmail.com>
2021-08-13 15:54:04 +1000

    pseudobam script added

--- bash/pseudopair_bam.sh
+++ bash/pseudopair_bam.sh
@@ -16,7 +16,7 @@ join -j 1 -o 1.1,1.2,1.3,1.4,1.5,1.6,2.3,2.4,1.9,1.10,1.11,1.12,1.13,1.14,1.15,1
 awk '{print $1"\t""83""\t"$3"\t"$4"\t"$5"\t"$6"\t"$8"\t"$8"\t"($8 + 142 - $4)"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14"\t"$15"\t"$16'} ${BAM1%%.bam}_tmp > ${BAM1%%.bam}_tmp2
 
 # combine header of R1 sam file
-cat <(samtools view -H $BAM1) ${BAM1%%.bam}_tmp2 > ${BAM1%%.bam}_pseudopaired.sam
+cat <(samtools view -H $BAM1) ${BAM1%%.bam}_tmp2 > ${BAM1%%_R1.bam}_pseudopaired.sam
 
 # cleanup tmp files
 rm ${BAM1%%.bam}_tmp ${BAM1%%.bam}_tmp2
